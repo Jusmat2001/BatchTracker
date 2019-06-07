e400d2bac299967051b984296e9e5e317468fae2
Justin Matlock <jmatlock@codeoneinc.com>
2019-06-07 10:31:52 -0600

    6/7 commit. pre oledb

--- BatchTracker/Form1.Designer.cs
+++ BatchTracker/Form1.Designer.cs
@@ -28,13 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Populate_btn = new System.Windows.Forms.Button();
+            this.SearchBox = new System.Windows.Forms.TextBox();
+            this.Search_btn = new System.Windows.Forms.Button();
+            this.practiceListView = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.CornflowerBlue;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle5;
+            this.dataGridView1.Location = new System.Drawing.Point(4, 35);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(717, 629);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Populate_btn
+            // 
+            this.Populate_btn.Location = new System.Drawing.Point(4, 670);
+            this.Populate_btn.Name = "Populate_btn";
+            this.Populate_btn.Size = new System.Drawing.Size(134, 27);
+            this.Populate_btn.TabIndex = 1;
+            this.Populate_btn.Text = "Queued Batches";
+            this.Populate_btn.UseVisualStyleBackColor = true;
+            this.Populate_btn.Click += new System.EventHandler(this.Populate_btn_Click);
+            // 
+            // SearchBox
+            // 
+            this.SearchBox.Location = new System.Drawing.Point(433, 12);
+            this.SearchBox.Name = "SearchBox";
+            this.SearchBox.Size = new System.Drawing.Size(288, 22);
+            this.SearchBox.TabIndex = 2;
+            // 
+            // Search_btn
+            // 
+            this.Search_btn.Location = new System.Drawing.Point(366, 12);
+            this.Search_btn.Name = "Search_btn";
+            this.Search_btn.Size = new System.Drawing.Size(61, 23);
+            this.Search_btn.TabIndex = 3;
+            this.Search_btn.Text = "Search";
+            this.Search_btn.UseVisualStyleBackColor = true;
+            this.Search_btn.Click += new System.EventHandler(this.Search_btn_Click);
+            // 
+            // practiceListView
+            // 
+            this.practiceListView.Location = new System.Drawing.Point(727, 72);
+            this.practiceListView.Name = "practiceListView";
+            this.practiceListView.Size = new System.Drawing.Size(63, 592);
+            this.practiceListView.TabIndex = 4;
+            this.practiceListView.UseCompatibleStateImageBehavior = false;
+            this.practiceListView.View = System.Windows.Forms.View.List;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Firebrick;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(724, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 34);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Active Practices";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Firebrick;
+            this.ClientSize = new System.Drawing.Size(802, 698);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.practiceListView);
+            this.Controls.Add(this.Search_btn);
+            this.Controls.Add(this.SearchBox);
+            this.Controls.Add(this.Populate_btn);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Batch Tracker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Populate_btn;
+        private System.Windows.Forms.TextBox SearchBox;
+        private System.Windows.Forms.Button Search_btn;
+        private System.Windows.Forms.ListView practiceListView;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- BatchTracker/Form1.cs
+++ BatchTracker/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,147 @@ namespace BatchTracker
         public Form1()
         {
             InitializeComponent();
+            
         }
+
+        string connectionString = "Server=3JS0082\\JUSTINSQLEXPRESS;Database=master;Integrated Security=true;";
+        string C1ConnString = "Server=SQL1\\SQL2005;DataBase=CodeOneMaster;UID=ReadOnlyUser;PWD=lij1Z96h";
+
+        List<string> practiceList = new List<string>();
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                practiceList = GetPractices();
+                practiceListView.GridLines = true;
+
+                if (practiceList!=null)
+                {
+
+                    foreach (string prac in practiceList)
+                    {
+                        practiceListView.Items.Add(prac);
+                    }
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public List<string> GetPractices()
+        {
+            string listQuery = "SELECT PracticeIdentifier FROM dbo.Practice Order by PracticeIdentifier";
+            List<string> list = new List<string>();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(C1ConnString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(listQuery, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                list.Add(reader.GetString(0));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return list;
+        }
+
+        private void Populate_btn_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT * FROM TestAPdb";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlDataAdapter dataadapter = new SqlDataAdapter(sql, connection))
+                    {
+                        DataSet ds = new DataSet();
+                        dataadapter.Fill(ds, "TestAPdb");
+                        connection.Close();
+                        dataGridView1.DataSource = ds;
+                        dataGridView1.DataMember = "TestAPdb";
+                    }
+                }
+                DataGridViewColumn column0 = dataGridView1.Columns[0];
+                column0.Width = 275;
+                DataGridViewColumn column1 = dataGridView1.Columns[1];
+                column1.Width = 100;
+                DataGridViewColumn column2 = dataGridView1.Columns[2];
+                column2.Width = 150;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Search_btn_Click(object sender, EventArgs e)
+        {
+            
+            string searchTerm = "";
+            string practice = "";
+            try
+            {
+                if (SearchBox.Text.Count() < 3)
+                {
+                    MessageBox.Show("Please enter at least 3 numbers to search.");
+                }
+                else
+                {
+                    searchTerm = SearchBox.Text;
+                    practice = searchTerm.Substring(0, 3);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            string sql = "SELECT * FROM TestAPdb WHERE Batch# LIKE '%"+searchTerm+"%'";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlDataAdapter dataadapter = new SqlDataAdapter(sql, connection))
+                    {
+                        DataSet ds = new DataSet();
+                        dataadapter.Fill(ds, "TestAPdb");
+                        connection.Close();
+                        dataGridView1.DataSource = ds;
+                        dataGridView1.DataMember = "TestAPdb";
+                    }
+                }
+                DataGridViewColumn column0 = dataGridView1.Columns[0];
+                column0.Width = 275;
+                DataGridViewColumn column1 = dataGridView1.Columns[1];
+                column1.Width = 100;
+                DataGridViewColumn column2 = dataGridView1.Columns[2];
+                column2.Width = 150;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        
     }
 }
